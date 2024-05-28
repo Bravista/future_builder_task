bddd26d4d444d424a594a2a02ca46f0915dd4f0e
Bravista <sekretz@web.de>
2024-05-28 15:09:14 +0200

    TS 552 fertiggestellt

--- lib/src/features/main_screen.dart
+++ lib/src/features/main_screen.dart
@@ -8,35 +8,74 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final TextEditingController zipController = TextEditingController();
+  Future<String>? cityFuture;
+
   @override
   void initState() {
     super.initState();
-    // TODO: initiate controllers
+    zipController.addListener(() {
+      setState(() {
+        cityFuture = null;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            children: [
-              const TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: "Postleitzahl"),
-              ),
-              const SizedBox(height: 32),
-              OutlinedButton(
-                onPressed: () {
-                  // TODO: implementiere Suche
-                },
-                child: const Text("Suche"),
-              ),
-              const SizedBox(height: 32),
-              Text("Ergebnis: Noch keine PLZ gesucht",
-                  style: Theme.of(context).textTheme.labelLarge),
-            ],
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 220,
+                ),
+                TextField(
+                  controller: zipController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Postleitzahl",
+                  ),
+                ),
+                const SizedBox(height: 55),
+                OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      cityFuture = getCityFromZip(zipController.text);
+                    });
+                  },
+                  child: const Text("Suche"),
+                ),
+                const SizedBox(height: 22),
+                FutureBuilder<String>(
+                  future: cityFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text(
+                        "Fehler: ${snapshot.error}",
+                        style: Theme.of(context).textTheme.labelLarge,
+                      );
+                    } else if (snapshot.hasData) {
+                      return Text(
+                        "Ergebnis: ${snapshot.data}",
+                        style: Theme.of(context).textTheme.labelLarge,
+                      );
+                    } else {
+                      return Text(
+                        "Ergebnis: Noch keine PLZ gesucht",
+                        style: Theme.of(context).textTheme.labelLarge,
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -45,13 +84,12 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   void dispose() {
-    // TODO: dispose controllers
+    zipController.dispose();
     super.dispose();
   }
 
   Future<String> getCityFromZip(String zip) async {
-    // simuliere Dauer der Datenbank-Anfrage
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 2));
 
     switch (zip) {
       case "10115":
@@ -65,6 +103,8 @@ class _MainScreenState extends State<MainScreen> {
       case "60311":
       case "60313":
         return 'Frankfurt am Main';
+      case "73207":
+        return "Plochingen";
       default:
         return 'Unbekannte Stadt';
     }
